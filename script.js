9c18b82acb139055f09510c34241f7278b2e011b
Maggie <maggiesun625@gmail.com>
2025-06-27 15:14:28 -0700

    added most of styling and overlay for js

---
+++ script.js
@@ -0,0 +1,39 @@
+//timerSelectin overlay div
+const overlay = document.getElementById("timerSelection");
+const customButton = document.getElementById("custom");
+const pomodoroButton = document.getElementById("pomodoro");
+const ldButton = document.getElementById("ld");
+const pfButton = document.getElementById("pf");
+
+function timerSelected(type){
+    overlayDone();
+    if(type == "custom"){
+
+    } else if(type == "pomodoro"){
+
+    } else if(type == "ld"){
+        
+    } else if(type == "pf"){
+
+    }
+
+}
+
+
+function pauseButton(){
+
+}
+
+function startButton(){
+
+}
+
+function stopButton(){
+
+}
+
+function overlayDone(){
+    overlay.style.display = "none";
+}
+
+
